092de4b460b31ecba4ae30193d736edc206722ad
syogo <syogo3256@gmail.com>
2021-06-26 03:03:29 +0900

    Add EditServlet

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -14,16 +14,16 @@ import models.Message;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class ShowServlet
+ * Servlet implementation class EditServlet
  */
-@WebServlet("/show")
-public class ShowServlet extends HttpServlet {
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ShowServlet() {
+    public EditServlet() {
         super();
     }
 
@@ -33,15 +33,19 @@ public class ShowServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		EntityManager em = DBUtil.createEntityManager();
 
-		//街頭のIDのメッセージ1件のみをデータベースから取得
+		//該当のIDのメッセージ1件のみをデータベースから取得
 		Message m = em.find(Message.class, Integer.parseInt(request.getParameter("id")));
 
 		em.close();
 
-		//メッセージデータをリクエストスコープにセットしてshow.jspを呼び出す
+		//メッセージ情報とセッションIDをリクエストスコープに登録
 		request.setAttribute("message", m);
+		request.setAttribute("_token", request.getSession().getId());
 
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/show.jsp");
+		//メッセージIDをセッションスコープに登録
+		request.getSession().setAttribute("message_id", m.getId());
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
 		rd.forward(request, response);
 
 	}
